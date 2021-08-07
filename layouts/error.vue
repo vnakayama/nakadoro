a77d7075d85cfc0dd8a4a4eda5db4cd5bd534278
Valentina Nakayama <valentina@poli.ufrj.br>
2021-08-07 16:58:06 -0300

    first commit

---
+++ layouts/error.vue
@@ -0,0 +1,45 @@
+<template>
+  <v-app>
+    <h1>
+      {{ error.statusCode }}
+    </h1>
+    <p v-if="error.statusCode === 404">
+      {{ pageNotFound }}
+    </p>
+    <p v-else>
+      {{ otherError }}
+    </p>
+    <NuxtLink to="/">
+      Home page
+    </NuxtLink>
+  </v-app>
+</template>
+
+<script>
+export default {
+  layout: 'default',
+  props: {
+    error: {
+      type: Object,
+      default: null
+    }
+  },
+  data () {
+    return {
+      pageNotFound: 'Ops! Parece que não tem nada aqui...',
+      otherError: 'Vish! Algo de errado não está certo...'
+    }
+  },
+  head () {
+    return {
+      title: "Ops! | Nakadoro"
+    }
+  }
+}
+</script>
+
+<style scoped>
+h1 {
+  font-size: 20px;
+}
+</style>
